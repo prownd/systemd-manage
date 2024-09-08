cb7d0ae105fa5a7c6d857e29d12dc15998bef00a
hanjinpeng <hanjinpeng127@gmail.com>
2024-09-09 01:07:09 +0800

    add build tar package for build.sh

--- dist/build.sh
+++ dist/build.sh
@@ -11,10 +11,14 @@ PKG_VERSION="1.2"
 PKG_VERSION_RELEASE="${PKG_VERSION}-1"
 BUILD_DIR="$( cd "$( dirname "$0" )" && pwd  )"
 TOP_DIR="$( cd "$( dirname "$0" )" && cd .. && pwd  )"
+OS_ARCH="`arch`"
+
+BUILD_TAR_NAME="${PKG_NAME}-${PKG_VERSION_RELEASE}-${OS_ARCH}"
+BUILD_TAR_DIR="${BUILD_TAR_NAME}"
 
 function  do_help()
 {
-     echo -e "\033[31m usage:\n $0  rpm|deb \033[0m"
+     echo -e "\033[31m usage:\n $0  rpm|deb|tar \033[0m"
      echo -e "\033[31m example:\n $0   rpm \033[0m"
      exit 1
 }
@@ -52,7 +56,6 @@ function build_rpm_package()
 
 function build_deb_package()
 {
-    #clean rpmbuild dir and regenerate rpmdev tree
     [ -d ${BUILD_DIR}/tmpbuilddir ] && rm -fr ${BUILD_DIR}/tmpbuilddir
     [ -d ${BUILD_DIR}/debbuild ] && rm -fr ${BUILD_DIR}/debbuild
     mkdir -p ${BUILD_DIR}/debbuild/{DEBIAN,}
@@ -95,6 +98,45 @@ function build_deb_package()
     [ -d ${BUILD_DIR}/tmpbuilddir ] && /bin/rm -fr  ${BUILD_DIR}/tmpbuilddir 
 }
 
+function build_tar_package()
+{
+    [ -d ${BUILD_DIR}/tmpbuilddir ] && rm -fr ${BUILD_DIR}/tmpbuilddir
+    [ -d ${BUILD_DIR}/tarbuild ] && rm -fr ${BUILD_DIR}/tarbuild
+    mkdir -p ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}
+
+    mkdir -p ${BUILD_DIR}/tmpbuilddir
+    mkdir -p ${BUILD_DIR}/tar-dir
+
+    pushd  ${BUILD_DIR}/tmpbuilddir 
+    qmake-qt5 $TOP_DIR/${PKG_NAME}.pro
+    make -j `nproc`
+    popd
+ 
+    # copy binnary file
+    install -d  ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}
+    install -m755 -p -D ${BUILD_DIR}/tmpbuilddir/${PKG_NAME}  ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}/
+
+    # copy .desktop file
+    install -d ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}/data/applications
+    install -m644 -p -D $TOP_DIR/data/${PKG_NAME}.desktop  ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}/data/applications/${PKG_NAME}.desktop
+
+    # copy icons
+    install -d ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}/data/icons
+    install -m644 -p -D $TOP_DIR/res/icons/* ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}/data/icons/
+
+    # license file
+    install -d ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}/data/license
+    install -m644 -p -D ${TOP_DIR}/LICENSE ${BUILD_DIR}/tarbuild/${BUILD_TAR_DIR}/data/license
+    
+    #build tar
+    cd  ${BUILD_DIR}/tarbuild/
+    tar -Jcvf ${BUILD_DIR}/tar-dir/${BUILD_TAR_NAME}-unknown-linux-gnu.tar.xz  ${BUILD_TAR_DIR}
+
+    #clean tmp build dir
+    [ -d ${BUILD_DIR}/tarbuild ]  && /bin/rm -fr  ${BUILD_DIR}/tarbuild 
+    [ -d ${BUILD_DIR}/tmpbuilddir ] && /bin/rm -fr  ${BUILD_DIR}/tmpbuilddir 
+}
+
 if [ $# != 1 ]; then
     do_help
 fi
@@ -105,6 +147,9 @@ if [ x"$1"  = x"rpm" ];then
 elif [ x"$1"  = x"deb" ];then
     #build deb
     build_deb_package
+elif [ x"$1"  = x"tar" ];then
+    #build tar
+    build_tar_package
 else
     do_help
     exit 1
